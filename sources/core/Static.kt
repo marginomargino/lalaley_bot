e8994b356243060f54b8d85b623ce71330011766
margino <vadim@widgetlabs.eu>
2023-11-21 13:01:57 +0400

    ignore missing .env

--- sources/core/Static.kt
+++ sources/core/Static.kt
@@ -1,6 +1,7 @@
 package core
 
 import io.github.cdimascio.dotenv.Dotenv
+import io.github.cdimascio.dotenv.dotenv
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import java.time.format.DateTimeFormatter
@@ -8,7 +9,7 @@ import java.util.*
 
 object Static {
     val clock = Clock.System
-    val env: Dotenv = Dotenv.load()
+    val env: Dotenv = dotenv{ ignoreIfMissing = true }
     val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.forLanguageTag("ru-RU"))
     val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm")
     val timeZone = TimeZone.of("Europe/Samara")
